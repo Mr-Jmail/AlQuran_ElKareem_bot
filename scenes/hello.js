fe8f6178e68696080fe1de1786826b95cbf8357b
Ваше Имя <you@example.com>
2022-04-08 00:55:09 +0300

    update

--- scenes/hello.js
+++ scenes/hello.js
@@ -8,10 +8,11 @@ bot.start(async (ctx) => {
     bot.hears('Абдуль-Басит Абдус-Самад (عبد الباسط عبد الصمد)', ctx => ctx.scene.enter('SecondReaderWizard'))
     bot.hears('Ахмад бин Али Аль-Аджми (أحمد علي العجمي)', ctx => ctx.scene.enter('ThirdReaderWizard'))
     bot.hears('Мохамед Махмуд Таблави (محمد محمود الطبلاوي)', ctx => ctx.scene.enter('FourthReaderWizard'))
+    bot.hears('Мухаммад Сиддик аль-Миншави (محمد صديق المنشاوي)', async (ctx) => ctx.scene.enter('FifthReaderWizard'))
 })
 
 const ReaderChoise = Keyboard.make([
-    ['Аль-Афаси (مشاري العفاسي)'], ['Абдуль-Басит Абдус-Самад (عبد الباسط عبد الصمد)'], ['Ахмад бин Али Аль-Аджми (أحمد علي العجمي)'], ['Мохамед Махмуд Таблави (محمد محمود الطبلاوي)']
+    ['Аль-Афаси (مشاري العفاسي)'], ['Абдуль-Басит Абдус-Самад (عبد الباسط عبد الصمد)'], ['Ахмад бин Али Аль-Аджми (أحمد علي العجمي)'], ['Мохамед Махмуд Таблави (محمد محمود الطبلاوي)'], ['Мухаммад Сиддик аль-Миншави (محمد صديق المنشاوي)']
 ]).oneTime()
 
 bot.command('change_reader', (ctx) => ctx.scene.enter('ChangeReaderWizard'))
